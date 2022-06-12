773e443ac8046ee9bb345df78ba48e1ef6ba3f22
Rich von Lehe <rhvonlehe@gmail.com>
2022-06-12 12:54:39 -0500

    some throwaway code to do a quickie test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 
+#include <Store.h>
+#include <iostream>
 
+using namespace std;
 
 int main(void)
 {
+    Store store;
 
+    auto families = store.getProductFamilies();
+
+    cout << "Product families: " << endl;
+
+    for (const auto& family : families)
+    {
+        cout << family <<endl;
+    }
 }
