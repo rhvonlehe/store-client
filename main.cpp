3cb5fd3f4a672a2f36c612343f3e8fb5bf89236b
Rich von Lehe <rhvonlehe@gmail.com>
2022-06-12 15:41:39 -0500

    Basic throwaway tests before implementing slightly better consumer and going multi-process.

--- main.cpp
+++ main.cpp
@@ -16,4 +16,22 @@ int main(void)
     {
         cout << family <<endl;
     }
+
+    auto products = store.getProductsInFamily(families[0]);
+
+    for (auto& product : products)
+    {
+        product.print();
+    }
+
+    auto toBuy = products[0];
+    cout << "Try to buy the below:" << endl;
+    toBuy.print();
+
+    store.purchaseProduct(products[0].id);
+
+    auto bought = store.getProductInfo(toBuy.id);
+    cout << "After purchase: " << endl;
+    bought.print();
+
 }
